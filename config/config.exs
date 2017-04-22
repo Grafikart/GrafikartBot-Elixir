6bda7d342c0414493986fe1ee98837b6b60c6bf1
Grafikart <contact@grafikart.fr>
2017-04-22 21:35:36 +0200

    Added new jobs

--- config/config.exs
+++ config/config.exs
@@ -78,6 +78,8 @@ Un bug / un problème avec le bot ? https://github.com/Grafikart/GrafikartBot-El
     "fullstack" => "305381229753139200",
     "backend"   => "305381310996676609",
     "frontend"  => "305381272832704514",
+    "devops"    => "305426040077942785",
+    "designer"  => "305425719515938836",
     "freelance" => "305381504479920129",
     "etudiant"  => "305381380802609163"
   }
