a5f60e789e1b4b459135c4a3d79ae8d237810629
Grafikart <contact@grafikart.fr>
2017-06-29 14:35:25 +0200

    Removed autoprune

--- lib/discordbot.ex
+++ lib/discordbot.ex
@@ -16,7 +16,7 @@ defmodule Discordbot do
         # Vérifie les nouveaux premiums
         worker(Discordbot.Tasks.Premium, [:premium_process]),
         # Auto prune les utilisateur au bout de X jours
-        worker(Discordbot.Tasks.Autoprune, [api_key, Application.get_env(:discordbot, :guild)]),
+        # worker(Discordbot.Tasks.Autoprune, [api_key, Application.get_env(:discordbot, :guild)]),
         # Parse le flux RSS à la recherche de nouvelles vidéos
         worker(Discordbot.Tasks.RSS, [api_key, Application.get_env(:discordbot, :guild)])
       ]
