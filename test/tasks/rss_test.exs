a0eda42deeb1d7f9fe304f0589535e7ceb439ea6
Grafikart <contact@grafikart.fr>
2017-08-25 20:13:05 +0200

    Fix insults

--- test/tasks/rss_test.exs
+++ test/tasks/rss_test.exs
@@ -10,18 +10,19 @@ defmodule Discordbot.Tasks.RSS.Test do
 
   def fake_entry do
     %{
-      title: "Tutoriel Laravel : Laravel : Echo",
+      title: "Tutoriel Laravel : Laravel Echo",
       link: "https://www.grafikart.fr/tutoriels/laravel/laravel-echo-websocket-890"
     }
   end
 
   test "Get last post" do
     last_post = RSS.last_post(fake_rss())
-    assert last_post = fake_entry()
+    assert last_post.title == fake_entry().title
+    assert last_post.link == fake_entry().link
   end
 
   test "Format message" do
-    assert "**<:grafikart:250692379638497280> Nouveau Tutoriel Laravel :** Laravel : Echo https://www.grafikart.fr/tutoriels/laravel/laravel-echo-websocket-890" = RSS.get_message(fake_entry())
+    assert "**<:grafikart:250692379638497280> Nouveau Tutoriel Laravel :** Laravel Echo https://www.grafikart.fr/tutoriels/laravel/laravel-echo-websocket-890" == RSS.get_message(fake_entry())
   end
 
 end
